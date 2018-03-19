b44c8c6dfb6a1bf31ffb1fc254826e6ba0bb0f1e
rosseji <rosseireland@gmail.com>
2018-03-19 12:34:47 +1100

    revert. Not sure what the issue is. Prettier? My code aint pretty...

--- transforms/favicon.js
+++ transforms/favicon.js
@@ -6,7 +6,8 @@ function transform() {
   return hstream({
     head: {
       _appendHtml: [
-        '<link rel="icon" type="image/png" href="/assets/icon.png"> <meta name="google-site-verification" content="sEpb5cdT-RwgGOJINpHIH8NGx5y1glv1hR_PkUqlRu0" />'
+        '<link rel="icon" type="image/png" href="/assets/icon.png">',
+        '<meta name="google-site-verification" content="sEpb5cdT-RwgGOJINpHIH8NGx5y1glv1hR_PkUqlRu0" />'
       ].join(''),
     },
   });
